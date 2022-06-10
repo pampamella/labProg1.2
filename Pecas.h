da475de5ba07c3cfe91811becd9953601a89ae28
Vítor Nascimento <vnascimento@inoa.com.br>
2022-06-10 09:48:24 -0300

    esboços de peças

--- Pecas.h
+++ Pecas.h
@@ -3,13 +3,64 @@
 
 #include<vector>
 #include<iostream>
-
+#include<Movimentos.h>
 
 
 using namespace std;
 
-class Pecas{
+enum tiposDePeca{
+    vazia = 0,
+    normal,
+    rainha
+};
 
+class Pecas{
+    protected:
+    tiposDePeca tipo;
+    Pecas(tiposDePeca tipo):tipo(tipo){};
+
+    public:
+    virtual Movimentos movimentar(){}
+    virtual vector<Movimentos> movimentosPossiveis(){}
+};
+
+class Vazia:public Pecas{
+    public:
+    Vazia():Pecas(vazia){}
+    Movimentos movimentar(){
+        Movimentos movimento();
+        return movimento;
+    }
+    vector<Movimentos> movimentosPossiveis(){
+        vector<Movimentos> movimentos;
+        return movimentos;
+    }
+};
+
+class Normal:public Pecas{
+    public:
+    Normal():Pecas(normal){}
+    Movimentos movimentar(){
+        Movimentos movimento();
+        return movimento;
+    }
+    vector<Movimentos> movimentosPossiveis(){
+        vector<Movimentos> movimentos;
+        return movimentos;
+    }
+};
+
+class Rainha:public Pecas{
+    public:
+    Rainha():Pecas(rainha){}
+    Movimentos movimentar(){
+        Movimentos movimento();
+        return movimento;
+    }
+    vector<Movimentos> movimentosPossiveis(){
+        vector<Movimentos> movimentos;
+        return movimentos;
+    }
 };
 
 
