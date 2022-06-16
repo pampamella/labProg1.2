315bc07c685d14509ba4a52e9513473432be27dc
pamella-silva <90103227+pampamella@users.noreply.github.com>
2022-06-16 00:19:26 -0300

    enunciado movimento

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ int main(){
     while(!Dama.fimJogo()){
         string posInicial, posFinal;
         do{
+            cout << "Insira o movimento no formato [letra inicial][numero inicial] [letra final][numero final], por exemplo: c3 d4" << endl;
+            cout << "Obs: Em casos de movimento de captura, insira a posicao da peca que deseja capturar como a posicao final do movimento, por exemplo: capturar peca em d4, movimento eh da forma c3 d4" << endl;
             cin >> posInicial>> posFinal;
         }
         while(!Dama.JogarA(posInicial, posFinal));
